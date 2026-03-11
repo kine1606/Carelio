aa831f8c0d0fe76cc8876593b9b2d1e63da3cb5f
kine1606 <137513694+kine1606@users.noreply.github.com>
2026-03-11 20:58:22 +0700

    init project

---
+++ src/main/java/com/amigoscode/carelio/CarelioApplication.java
@@ -0,0 +1,15 @@
+package com.amigoscode.carelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarelioApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CarelioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amigoscode/carelio/CarelioApplicationTests.java
@@ -0,0 +1,15 @@
+package com.amigoscode.carelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarelioApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
