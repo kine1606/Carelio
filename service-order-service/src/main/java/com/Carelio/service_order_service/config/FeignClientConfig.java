b4f5a2730d1140346043eb6a60d9c83d37247dc6
kine1606 <137513694+kine1606@users.noreply.github.com>
2026-06-23 16:43:07 +0700

    modify api order client into internal ( unblock 401 error)

---
+++ service-order-service/src/main/java/com/Carelio/service_order_service/config/FeignClientConfig.java
@@ -0,0 +1,4 @@
+package com.Carelio.service_order_service.config;
+
+public class FeignClientConfig {
+}
--- worker-service/src/main/java/com/Carelio/worker_service/client/OrderClient.java
+++ worker-service/src/main/java/com/Carelio/worker_service/client/OrderClient.java
@@ -3,24 +3,24 @@ package com.Carelio.worker_service.client;
 import com.Carelio.worker_service.client.dto.OrderResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "order-client", contextId = "workerServiceOrderClient" , url = "${order.service.url}")
-public interface OrderClient
-{
-    @PatchMapping("/api/service-orders/{orderId}/accept")
+@FeignClient(name = "service-order-service", contextId = "workerServiceOrderClient", url = "${order.service.url}")
+public interface OrderClient {
+
+    @PatchMapping("/api/internal/service-orders/{orderId}/accept")
     void acceptOrder(@PathVariable("orderId") Long orderId,
                      @RequestParam("workerId") Long workerId);
 
-    @PatchMapping("/api/service-orders/{orderId}/complete")
-    void completeOrder(@PathVariable("orderId") Long orderId,
-                         @RequestParam("workerId") Long workerId);
-
-    @PatchMapping("/api/service-orders/{orderId}/start")
+    @PatchMapping("/api/internal/service-orders/{orderId}/start")
     void startOrder(@PathVariable("orderId") Long orderId,
                     @RequestParam("workerId") Long workerId);
+
+    @PatchMapping("/api/internal/service-orders/{orderId}/complete")
+    void completeOrder(@PathVariable("orderId") Long orderId,
+                       @RequestParam("workerId") Long workerId);
 
     @GetMapping("/api/internal/service-orders/{orderId}")
     OrderResponse getOrder(@PathVariable("orderId") Long orderId);
