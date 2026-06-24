68d06a0f1f1479c790f010fd0a3ee9dee6167040
kine1606 <137513694+kine1606@users.noreply.github.com>
2026-06-24 16:22:26 +0700

    fix big deprecated GenericJackson2JsonRedisSerializer in RedisConfig file

--- service-order-service/src/main/java/com/Carelio/service_order_service/config/CacheConfig.java
+++ service-order-service/src/main/java/com/Carelio/service_order_service/config/CacheConfig.java
@@ -3,8 +3,8 @@ package com.Carelio.service_order_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -15,11 +15,11 @@ public class CacheConfig {
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMillis(6000000)) // Thời gian sống của cache lấy từ cấu hình của bạn
+                .entryTtl(Duration.ofMillis(6000000))
                 .disableCachingNullValues()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        .fromSerializer(RedisSerializer.json()));
     }
 }
