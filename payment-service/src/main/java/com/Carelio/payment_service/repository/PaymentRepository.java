612d27c9f8a8d3bfec032517a952892aa652a63c
kine1606 <137513694+kine1606@users.noreply.github.com>
2026-06-25 00:21:25 +0700

    set time out for international service call

--- payment-service/src/main/java/com/Carelio/payment_service/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/Carelio/payment_service/repository/PaymentRepository.java
@@ -1,13 +1,14 @@
 package com.Carelio.payment_service.repository;
 
-import aj.org.objectweb.asm.commons.Remapper;
 import com.Carelio.payment_service.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     boolean existsByOrderIdAndStatus(Long id, String success);
 
-    Remapper findTopByOrderIdOrderByIdDesc(Long orderId);
+    Optional<Payment> findTopByOrderIdOrderByIdDesc(Long orderId);
 }
